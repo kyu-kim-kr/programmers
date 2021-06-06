0cd939bbbb1681e05af518fa29a58518af7728a3
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-06-06 17:48:03 +1200

    [Level2/Problem3] 50 / 50 점

---
+++ src/skillcheck/leveltwo/Problem3.java
@@ -0,0 +1,21 @@
+package skillcheck.leveltwo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public String solution(String s) {
+        String[] array = s.split(" ");
+
+        List<Integer> integerList = new ArrayList<>();
+        for (String element : array) {
+            integerList.add(Integer.valueOf(element));
+        }
+
+        String answer = Collections.min(integerList) + " " + Collections.max(integerList);
+
+
+        return answer;
+    }
+}
