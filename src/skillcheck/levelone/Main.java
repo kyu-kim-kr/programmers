5bcaa21e44214033cca6a7883dd85cca2ecfc6b2
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-06-06 13:02:33 +1200

    [Level1/Problem2] 42.5점/50점 코드

--- src/skillcheck/levelone/Main.java
+++ src/skillcheck/levelone/Main.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Problem1 levelOne = new Problem1();
-        System.out.println(Arrays.toString(levelOne.solution(2, 5)));
+        Problem1 problem1 = new Problem1();
+        System.out.println(Arrays.toString(problem1.solution(3, 12)));
+        System.out.println(Arrays.toString(problem1.solution(2, 5)));
+
+        Problem2 problem2 = new Problem2();
+        System.out.println(Arrays.toString(problem2.solution(2, 5)));
+        System.out.println(Arrays.toString(problem2.solution(4, 3)));
+        System.out.println(Arrays.toString(problem2.solution(-4, 2)));
     }
 }
--- src/skillcheck/levelone/Problem2.java
+++ src/skillcheck/levelone/Problem2.java
@@ -1,4 +1,23 @@
 package skillcheck.levelone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem2 {
+    public int[] solution(int x, int n) {
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(x * i);
+        }
+
+        return toIntArray(list);
+    }
+
+    private int[] toIntArray(List<Integer> list) {
+        int[] array = new int[list.size()];
+        for (int i = 0; i < array.length; i++)
+            array[i] = list.get(i);
+        return array;
+    }
 }
